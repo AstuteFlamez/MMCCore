44207d3663f4cb48633f615e1b2f02ae915a0d2f
AstuteFlamez <perosinal@gmail.com>
2023-06-15 21:36:43 -0500

    v1.2.4

--- src/main/java/mandomc/mmccore/commands/Yaw.java
+++ src/main/java/mandomc/mmccore/commands/Yaw.java
@@ -10,7 +10,7 @@ public class Yaw implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-
+        //test
         if(!(sender instanceof Player)) {return true;}
 
         if(command.getName().equalsIgnoreCase("yaw")){
